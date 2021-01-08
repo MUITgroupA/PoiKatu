e0ad2a341b9bb7f95443c727793b628963886aac
Yutaro Yamashita <yyyamataro0314@gmail.com>
2021-01-08 11:30:18 +0900

    ポイントゲーム機能を修正

--- front/js/main.js
+++ front/js/main.js
@@ -2,7 +2,23 @@
 
 {
 
+  function PointAdd(id,val){ //ポイントの増加
+      $.ajax({
+          type: 'PUT',
+          timeout: 10000,
+          url: "https://8wq7kom4q2.execute-api.ap-northeast-1.amazonaws.com/v2?id="+id+"&val="+val,
+          headers: {
+              "accept": "application/json"
+          },
+          dataType: 'json',
+          cache: false,
+      }).done(function(e){
+          console.log(e);  
+      });
+  }
+
   const btn = document.getElementById('btn');
+  var flag = false;
   
   btn.addEventListener('click',() => {
     // const results = ['大吉','吉','大凶','中吉','大凶','大凶','大凶','大凶','大凶','大凶','大凶','大凶'];
@@ -10,13 +26,22 @@
     // btn.textContent = results[n];
     // btn.textContent = results[Math.floor(Math.random() * results.length)];
     const n = Math.random();
-    if (n < 0.05){
-      btn.textContent = '100';  //5%
-
-    }else if (n < 0.2){
-      btn.textContent = '50';  //15%
-    }else{
-      btn.textContent = '10'; //80%
+    if(flag == false){
+      flag = true;
+      var bonus;
+      if (n < 0.05){
+        //btn.textContent = '100';  //5%
+        bonus = 10;
+      }else if (n < 0.2){
+        //btn.textContent = '50';  //15%
+        bonus = 5;
+      }else{
+        //btn.textContent = '10'; //80%
+        bonus = 1;
+      }
+      $('#message').text(bonus+"ポイント増加しました！");
+      btn.textContent = bonus;
+      PointAdd(1,bonus);
     }
   });
   
